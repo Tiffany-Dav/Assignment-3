54e0ce09a908a38dcc6fd07c9adf7cf5736cd9a1
TIffanyDavis <trd222@email.vccs.edu>
2023-09-12 20:41:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+           
+
+            Console.WriteLine("Patient Log");
+            Console.WriteLine();
+
+            Console.WriteLine("Press 1 for Sick Appointmnet");
+            Console.WriteLine("Press 2 for Check up");
+
+
+            String sickAppt, checkUp;
+
+            sickAppt = Console.ReadLine();
+            checkUp = Console.ReadLine();
+
+            if(sickAppt == 1")
+            {
+                if(checkUp == "b")
+                {
+                  
+                }
+            }
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Is the patient a:");
+            Console.WriteLine("a) Adult");
+            Console.WriteLine("b) Child");
+            Console.WriteLine();
+
+            const double CHILD_SICKVISIT_PRICE = 50.00;
+            const double ADULT_SICKVISIT_PRICE = 75.00;
+            const double LABS_PRICE = 25.00;
+            const double CHILD_CHECKUP_PRICE = 75.00;
+            const double ADULT_CHECKUP_PRICE = 100.00;
+           
+            
+
+            double price1 = CHILD_SICKVISIT_PRICE;
+            double price2 = ADULT_SICKVISIT_PRICE;
+            double price3 = CHILD_CHECKUP_PRICE;
+            double price4 = ADULT_CHECKUP_PRICE;
+            double price5 = LABS_PRICE;
+
+
+
+          
+
+
+            Console.ReadKey();
+        
+         
+               
+                
+
+           
+
+       
+
+        }
+    }
+}
